40362a511d6126ac204f9552874ccedcad123572
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-26 18:55:02 +0200

    created link with php / api

---
+++ api.php
@@ -0,0 +1,11 @@
+
+<?php
+require_once __DIR__ . '/database.php';
+
+header('Content-Type: application/json');
+
+$response = $database;
+
+echo json_encode($response);
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/database.php';
+
+foreach ($database as $disco) {
+
+    echo strtoupper($disco['title']);
+
+    foreach($disco as $key => $value){
+        echo "<br>";
+        echo "$key => $value";
+        echo "<br>";
+    }
+    echo "<br>";
+}
+?>
